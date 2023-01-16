70f4252e862d809be6c7dd1b072610d1ee9958a7
Edwin6666 <edwinmendoza0620@gmail.com>
2023-01-16 13:59:52 -0400

    function for rules

--- rock,paper or scissors.py
+++ rock,paper or scissors.py
@@ -1,6 +1,7 @@
 import random
 
-def play():
+
+def play_game():
     user=input("please type any of the following words (pa for paper, ro for rock, and sci for scissors).\n").lower()
     computer=random.choice(["pa","sci","ro"])
 
@@ -8,7 +9,24 @@ def play():
          return "tie!"
     
     
-    
+    if user_won():
+        return "You Win"
+
+    return "You Lose"
+
+
+def user_won(player,opponent):
+
+    if ((player=="pa" and opponent =="ro")
+      or (player=="ro" and opponent =="sci")
+      or (player=="sci" and opponent =="pa")):
+      return True
+      
+    else:  return False
+
+
+
+
     
 
 
