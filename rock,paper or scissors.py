2256221eed6ec3f9ea0fef3a4d76914d713b78dc
Edwin6666 <edwinmendoza0620@gmail.com>
2023-01-16 13:06:26 -0400

    Add: functionality to receive an option and the computer returns a random

--- rock,paper or scissors.py
+++ rock,paper or scissors.py
@@ -1,3 +1,16 @@
 import random
 
+def play():
+    user=input("please type any of the following words (pa for paper, ro for rock, and sci for scissors).\n").lower()
+    computer=random.choice(["pa","sci","ro"])
 
+    if user==computer:
+         return "tie!"
+    
+    
+    
+    
+
+
+
+    
